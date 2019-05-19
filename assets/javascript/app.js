0c2784a66d51583550a8d3d414f3d42259b172b6
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-19 15:55:00 -0500

    added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,58 @@
-$( document ).ready(function() {
-    
-//start button 
+$(document).ready(function() {
+$("#start").on("click", start);
+$("#finish").on("click", stop);
 
 
+var intervalId;
+var clockRunning = false;
+var time = 90;
 
+// display timer 
+
+$("#timer").text("01:30");
 
 //timer to count down time remaining in game
-    // display timer 
-    //game should end when time runs out
+
+function start() {
+    if (!clockRunning) {
+      intervalId = setInterval(count, 1000);
+      clockRunning = true;
+    }
+  }
+
+function count() {
+    time--;
+    var converted = timeConverter(time);
+    console.log(converted);
+    $("#timer").text(converted);
+}
+
+function timeConverter(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+  
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    return minutes + ":" + seconds;
+  }
+
+  function stop() {
+
+    clearInterval(intervalId);
+    clockRunning = false;
+  }
+
+
+
+
 
 //end of game should display questions correct/incorrect 
 
