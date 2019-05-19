52db7bb8ec6016e53db3beb8221db675d470e7ad
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-19 14:57:57 -0500

    psuedocode and buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+    
+//start button 
+
+
+
+
+//timer to count down time remaining in game
+    // display timer 
+    //game should end when time runs out
+
+//end of game should display questions correct/incorrect 
+
+
+// question and answer system
+
+
+
+//submit button
+
+});
